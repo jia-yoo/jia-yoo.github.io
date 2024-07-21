fe32dab16e2111a001420109bdeda01173e742bb
jia-yoo <jiayoo.dev@gmail.com>
2024-07-22 01:57:42 +0900

    이력서 추가

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav ul li a');
+const icons = document.querySelectorAll(".contact_icon");
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+icons.forEach((icon) => {
+  icon.addEventListener("click", function (event) {
+    if (event.target.classList.contains("resume_icon")) {
+      window.open("/images/유지아 이력서.pdf", "_blank");
+    } else if (event.target.classList.contains("email_icon")) {
+      console.log("email");
+      location.href = "mailto:jiayoo.dev@gmail.com";
+    } else if (event.target.classList.contains("github_icon")) {
+      window.open("https://github.com/jia-yoo", "_blank");
+    }
+  });
+});
 
-            window.scrollTo({
-                top: targetElement.offsetTop - 50,
-                behavior: 'smooth'
-            });
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const sidebar = document.getElementById("sidebar");
+  const aboutSection = document.querySelector("#about");
+  const projectSection = document.querySelector("#project");
+
+  hamburgerMenu.addEventListener("click", function () {
+    if (sidebar.style.width === "250px") {
+      closeSidebar();
+    } else {
+      openSidebar();
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !sidebar.contains(event.target) &&
+      !hamburgerMenu.contains(event.target)
+    ) {
+      closeSidebar();
+    }
+  });
+
+  function openSidebar() {
+    sidebar.style.width = "250px";
+  }
+
+  function closeSidebar() {
+    sidebar.style.width = "0";
+  }
+
+  window.addEventListener("scroll", function () {
+    const aboutSectionPos = aboutSection.getBoundingClientRect().top;
+    const projectSectionPos = projectSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight / 1.3;
+
+    console.log(screenPos);
+    console.log(projectSectionPos);
+
+    if (aboutSectionPos < screenPos) {
+      aboutSection.classList.add("show");
+      aboutSection.classList.remove("hidden");
+    }
+
+    if (projectSectionPos < screenPos) {
+      projectSection.classList.add("show");
+      projectSection.classList.remove("hidden");
+    }
+  });
 });
